82582fa9b2a252b5f7e6ba3da42d3efa6ec0b416
KUCKLU <kuck1u@visualive.jp>
2016-08-17 01:26:31 +0900

    no message

--- functions.php
+++ functions.php
@@ -8,11 +8,7 @@
 
 if ( ! function_exists( 'yetibreath_setup' ) ) :
 	/**
-	 * Sets up theme.
 	 * Sets up theme defaults and registers support for various WordPress features.
-	 * Note that this function is hooked into the aftercomponentsetup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
 	 */
 	function yetibreath_setup() {
 		/**
