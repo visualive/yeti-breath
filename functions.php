297c7be51967cd591c42ff4bb7c5b0d0e0d3868f
KUCKLU <kuck1u@visualive.jp>
2016-08-17 09:42:31 +0900

    わざとエラーが出来るコードを書いてみる

--- functions.php
+++ functions.php
@@ -49,9 +49,9 @@ if ( ! function_exists( 'yetibreath_setup' ) ) :
 		/**
 		 * This theme uses wp_nav_menu() in one location.
 		 */
-		register_nav_menus( array(
+		register_nav_menus( [
 			'menu-1' => esc_html__( 'Top', 'yetibreath' ),
-		) );
+		] );
 
 		/**
 		 * Add support for core custom logo.
